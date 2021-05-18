6190e809e304b71b700e5abcf43c147dccdc2235
Angel <angeljcampo@gmail.com>
2021-05-17 21:53:22 -0400

    first commit

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,19 @@
+class ProjectsController < ApplicationController
+  http_basic_authenticate_with name: 'desafiovamoscontodo', password: 'XAHTJEAS23123%23', only: :dashboard
+
+
+  def new
+    @project = Project.new()
+  end
+  def create
+    @project = Project.create(name: params[:name], description: params[:description], start_date: params[:start_date], end_date: params[:end_date], status: params[:status])
+  end
+
+  def dashboard
+    if params[:status].present?
+      @project = Project.where('status = ?', params[:status])
+    else
+      @project = Project.all
+    end
+  end
+end
